b5c836a1e99885798c24cb298cdc6a7fb1f1dcc2
Diego Almeida <diego.almeida@memed.com.br>
2023-06-08 09:38:39 -0300

    feat: add header and image optimization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from "next/app";
+import Image from "next/image";
 import { globalStyles } from "../styles/global";
+import { Container, Header } from "../styles/pages/app";
+import logoSVG from '../assets/svg/logo.svg'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoSVG} alt="Logo Ignite" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
