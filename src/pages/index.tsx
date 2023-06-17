e33a60ca8bef8cd2e2f82e2690a1279dec9def22
Diego Almeida <diego.almeida@memed.com.br>
2023-06-17 17:23:28 -0300

    feat: add link navigation to page product detail

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
+import { GetStaticProps } from "next";
 import Image from "next/image";
+import Link from "next/link";
+import Stripe from "stripe";
 import { useKeenSlider } from 'keen-slider/react'
+
+import { stripe } from "../lib/stripe";
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import 'keen-slider/keen-slider.min.css'
-import { GetStaticProps } from "next";
-import { stripe } from "../lib/stripe";
-import Stripe from "stripe";
+
 
 interface IHomeProps {
   products: {
@@ -28,13 +31,15 @@ export default function Home({ products }: IHomeProps) {
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => (
-        <Product key={product.id} className="keen-slider__slide">
-          <Image src={product.imageUrl} width={520} height={480} alt="" />
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
+        <Link key={product.id} href={`/product/${product.id}`}>
+          <Product className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        </Link>
       ))}
     </HomeContainer>
   )
